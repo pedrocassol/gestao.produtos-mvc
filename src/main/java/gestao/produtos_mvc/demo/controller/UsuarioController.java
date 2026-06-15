07a7b569a1336e9e35d7165603ef8cdd60245524
pedrocassol <cassolpedro61@gmail.com>
2026-06-15 14:59:42 -0300

    Create UsuarioController.java

---
+++ src/main/java/gestao/produtos_mvc/demo/controller/UsuarioController.java
@@ -0,0 +1,31 @@
+package gestao.produtos_mvc.demo.controller;
+
+import model.Usuario;
+import gestao.produtos_mvc.demo.dao.UsuarioDAO;
+import gestao.produtos_mvc.demo.service.UsuarioService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+    private UsuarioService service = new UsuarioService();
+
+    @PostMapping
+    public String cadastrar(@RequestParam String name, @RequestParam String email, @RequestParam String senha, Model model){
+
+        Usuario u = new Usuario(nome, email, senha, true);
+        boolean sucesso = service.inserir(u);
+
+        if(sucesso){
+            return "redirect:/login";
+        }else{
+            model.addAttribute("erro", "Erro ao cadastrar usuário");
+            return "cadastro";
+        }
+    }
+}
