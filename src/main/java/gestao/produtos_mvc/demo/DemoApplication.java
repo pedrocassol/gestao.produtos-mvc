9b15cb72a3b34628dfb8ef22620386e42074e90f
pedrocassol <cassolpedro61@gmail.com>
2026-06-15 14:08:35 -0300

    add

---
+++ src/main/java/gestao/produtos_mvc/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package gestao.produtos_mvc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gestao/produtos_mvc/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package gestao.produtos_mvc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
